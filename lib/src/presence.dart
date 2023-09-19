6606b9df4438675f82107a38663b05cf126f7b6a
조재용 <jo.jaeyong@purplit.com>
2023-09-19 20:42:42 +0900

    presence 수정

--- lib/src/presence.dart
+++ lib/src/presence.dart
@@ -8,6 +8,10 @@ import 'dart:async';
 import 'channel.dart';
 import 'message.dart';
 
+// typedef Presence = Map<String, dynamic>;
+typedef PresenceState = Map<String, Presence>;
+typedef PresenceDiff = Map<String, PresenceState>;
+
 typedef JoinHandler = void Function(
   String key,
   Presence? current,
@@ -118,57 +122,63 @@ class PhoenixPresence {
 
   // Process new Presence messages.
   void _onMessage(Message message) {
-    // Processing of 'state' events.
-    if (message.event.value == stateEventName) {
-      _joinRef = channel.joinRef;
-      final newState = _decodeStateFromPayload(message.payload!);
-      state = _syncState(state, newState);
-      for (final diff in pendingDiffs) {
-        state = _syncDiff(state, diff);
-      }
-      pendingDiffs = [];
-      onSync();
-
-      // Processing of 'diff' events.
-    } else if (message.event.value == diffEventName) {
-      final diff = _decodeDiffFromPayload(message.payload!);
-      if (inPendingSyncState) {
-        pendingDiffs.add(diff);
-      } else {
-        state = _syncDiff(state, diff);
+    final payload = message.payload;
+    if (payload != null) {
+      // Processing of 'state' events.
+      if (message.event.value == stateEventName) {
+        _joinRef = channel.joinRef;
+        state = _syncState(state, _decodeStateFromPayload(payload));
+        for (final diff in pendingDiffs) {
+          state = _syncDiff(state, diff);
+        }
+        pendingDiffs = [];
         onSync();
+
+        // Processing of 'diff' events.
+      } else if (message.event.value == diffEventName) {
+        final diff = _decodeDiffFromPayload(payload);
+        if (inPendingSyncState) {
+          pendingDiffs.add(diff);
+        } else {
+          state = _syncDiff(state, diff);
+          onSync();
+        }
       }
     }
   }
 
   /// Generates a "state" map with [Presence] objects
   /// from a serialized message payload.
-  Map<String, Presence> _decodeStateFromPayload(Map<String, dynamic> payload) {
-    return payload.map(
-        (key, metas) => MapEntry(key, Presence.fromJson(key, {key: metas})));
+  PresenceState _decodeStateFromPayload(Map<String, dynamic> payload) {
+    return payload.map((key, data) => MapEntry(key, Presence(data)));
   }
 
   /// Generates a "diff" map with [Presence] objects
   /// from a serialized message payload.
-  Map<String, Map<String, Presence>> _decodeDiffFromPayload(
-      Map<String, dynamic> payload) {
-    return payload.map((key, presence) {
-      if ((presence as Map).isEmpty) {
-        return MapEntry(key, <String, Presence>{});
-      }
-      final presenceKey = (presence as Map<String, dynamic>).keys.first;
-      return MapEntry(
-          key, {presenceKey: Presence.fromJson(presenceKey, presence)});
-    });
+  PresenceDiff _decodeDiffFromPayload(Map<String, dynamic> payload) {
+    return payload
+        .map((key, state) => MapEntry(key, _decodeStateFromPayload(state)));
+    //     if ((presences as Map<String, dynamic>).isEmpty) {
+    //       return MapEntry(key, <String, Presence>{});
+    //     }
+    //     try {
+    //       return MapEntry(key, presences.map((presenceKey, presence) {
+    //         return MapEntry(
+    //             presenceKey, Presence.fromJson(presenceKey, presence));
+    //       }));
+    //     } catch (e) {
+    //       return MapEntry(key, <String, Presence>{});
+    //     }
+    //   });
   }
 
   ///  Used to sync the list of presences on the server
   ///  with the client's state. Will call [onJoin] and [onLeave] callbacks
   ///  to react to changes in the client's local presences across
   ///  disconnects and reconnects with the server.
-  Map<String, Presence> _syncState(
-    Map<String, Presence> currentState,
-    Map<String, Presence> newState,
+  PresenceState _syncState(
+    PresenceState currentState,
+    PresenceState newState,
   ) {
     final state = _clone(currentState);
     final joins = <String, Presence>{};
@@ -182,14 +192,14 @@ class PhoenixPresence {
     _map(newState, (key, newPresence) {
       if (state.containsKey(key)) {
         final currentPresence = state[key]!;
-        final newRefs = (newPresence.metas).map((m) => m.phxRef).toSet();
-        final curRefs = (currentPresence.metas).map((m) => m.phxRef).toSet();
+        final newRefs = newPresence.metas.map((m) => m.phxRef).toSet();
+        final curRefs = currentPresence.metas.map((m) => m.phxRef).toSet();
 
-        final joinedMetas = (newPresence.metas)
+        final joinedMetas = newPresence.metas
             .where((m) => !curRefs.contains(m.phxRef))
             .toList();
 
-        final leftMetas = (currentPresence.metas)
+        final leftMetas = currentPresence.metas
             .where((m) => !newRefs.contains(m.phxRef))
             .toList();
 
@@ -198,7 +208,7 @@ class PhoenixPresence {
           joins[key]!.metas = joinedMetas;
         }
         if (leftMetas.isNotEmpty) {
-          leaves[key] = currentPresence.clone();
+          leaves[key] = Presence.clone(currentPresence);
           leaves[key]!.metas = leftMetas;
         }
       } else {
@@ -212,9 +222,9 @@ class PhoenixPresence {
   ///  events from the server, as they happen. Will call [onJoin]
   ///  and [onLeave] callbacks to react to a user
   ///  joining or leaving from a device.
-  Map<String, Presence> _syncDiff(
-    Map<String, Presence> currentState,
-    Map<String, Map<String, Presence>> diff,
+  PresenceState _syncDiff(
+    PresenceState currentState,
+    PresenceDiff diff,
   ) {
     final state = _clone(currentState);
 
@@ -225,22 +235,22 @@ class PhoenixPresence {
       final currentPresence = state[key];
       state[key] = newPresence;
       if (currentPresence != null) {
-        final joinedRefs = (state[key]!.metas).map((m) => m.phxRef).toSet();
-        final curMetas = (currentPresence.metas)
-            .where((m) => !joinedRefs.contains(m.phxRef));
-        (state[key]!.metas).insertAll(0, curMetas);
+        final joinedRefs = state[key]!.metas.map((m) => m.phxRef).toSet();
+        final curMetas =
+            currentPresence.metas.where((m) => !joinedRefs.contains(m.phxRef));
+        state[key]!.metas.insertAll(0, curMetas);
       }
       onJoin(key, currentPresence, newPresence);
     });
     _map(leaves, (key, leftPresence) {
       final currentPresence = state[key];
       if (currentPresence == null) return;
-      final refsToRemove = (leftPresence.metas).map((m) => m.phxRef).toSet();
+      final refsToRemove = leftPresence.metas.map((m) => m.phxRef).toSet();
       currentPresence.metas = (currentPresence.metas)
           .where((m) => !refsToRemove.contains(m.phxRef))
           .toList();
       onLeave(key, currentPresence, leftPresence);
-      if ((currentPresence.metas).isEmpty) {
+      if (currentPresence.metas.isEmpty) {
         state.remove(key);
       }
     });
@@ -248,7 +258,7 @@ class PhoenixPresence {
   }
 
   List<dynamic> _map(
-    Map<String, Presence> presences,
+    PresenceState presences,
     dynamic Function(String, Presence) mapper,
   ) {
     if (presences.isNotEmpty) {
@@ -261,42 +271,42 @@ class PhoenixPresence {
   }
 
   /// Clones a Map<String, Presence> object. Useful for cloning states.
-  Map<String, Presence> _clone(Map<String, Presence> presences) {
-    return presences.map((key, value) => MapEntry(key, value.clone()));
+  PresenceState _clone(PresenceState presences) {
+    return {...presences};
   }
 }
 
 /// Class that encapsulate all presence events for
 /// a specific [key] as a list of metadatas events [metas].
-class Presence {
-  Presence.fromJson(this.key, Map<String, dynamic> events)
-      : data = {...events[key]},
-        metas = List<Map<String, dynamic>>.from(events[key]['metas'])
-            .map((meta) => PhoenixPresenceMeta.fromJson(meta))
-            .toList();
+// class Presence {
+//   Presence.fromJson(this.key, Map<String, dynamic> events)
+//       : data = {...events[key]},
+//         metas = List<Map<String, dynamic>>.from(events[key]['metas'])
+//             .map((meta) => PhoenixPresenceMeta.fromJson(meta))
+//             .toList();
 
-  /// Identify the presence, typically a userId.
-  final String key;
+//   /// Identify the presence, typically a userId.
+//   final String key;
 
-  /// The raw data
-  final Map<String, dynamic> data;
+//   /// The raw data
+//   final Map<String, dynamic> data;
 
-  /// A list of all events metadatas for this Presence [key].
-  List<PhoenixPresenceMeta> metas;
+//   /// A list of all events metadatas for this Presence [key].
+//   List<PhoenixPresenceMeta> metas;
 
-  Presence clone() {
-    final json = _toJson();
-    final key = json.keys.first;
-    return Presence.fromJson(key, json);
-  }
+//   Presence clone() {
+//     final json = _toJson();
+//     final key = json.keys.first;
+//     return Presence.fromJson(key, json);
+//   }
 
-  Map<String, dynamic> _toJson() {
-    final json = <String, dynamic>{};
-    json[key] = <String, dynamic>{};
-    json[key]['metas'] = metas.map((meta) => meta._toJson()).toList();
-    return json;
-  }
-}
+//   Map<String, dynamic> _toJson() {
+//     final json = <String, dynamic>{};
+//     json[key] = <String, dynamic>{};
+//     json[key]['metas'] = metas.map((meta) => meta._toJson()).toList();
+//     return json;
+//   }
+// }
 
 /// Class that encapsulate the various metadata for a [Presence] event.
 /// This class only implements the default metadata field [phxRef], all
@@ -304,22 +314,37 @@ class Presence {
 /// the 'phxRef' field). It can be extended with custom field as
 /// required, see 'example/flutter_presence_app' for an example on
 /// how to implement this in your code.
-class PhoenixPresenceMeta {
-  PhoenixPresenceMeta.fromJson(Map<String, dynamic> meta)
-      : data = {...meta},
-        phxRef = meta['phx_ref'];
+// class PhoenixPresenceMeta {
+//   PhoenixPresenceMeta.fromJson(Map<String, dynamic> meta)
+//       : data = {...meta},
+//         phxRef = meta['phx_ref'];
 
-  /// The raw data associated with the meta.
-  final Map<String, dynamic> data;
+//   /// The raw data associated with the meta.
+//   final Map<String, dynamic> data;
 
-  /// The [Presence] event reference on the backend Phoenix server.
+//   /// The [Presence] event reference on the backend Phoenix server.
+//   final String phxRef;
+
+//   /// Clones a [PhoenixPresenceMeta] object
+//   PhoenixPresenceMeta clone() {
+//     final json = _toJson();
+//     return PhoenixPresenceMeta.fromJson(json);
+//   }
+
+//   Map<String, dynamic> _toJson() => data;
+// }
+
+class Presence {
+  Presence(this.data)
+      : metas = (data['metas'] as List).map((m) => PresenceMeta(m)).toList();
+  Presence.clone(Presence original) : this(original.data);
+
+  Map<String, dynamic> data;
+  List<PresenceMeta> metas;
+}
+
+class PresenceMeta {
+  PresenceMeta(Map meta) : phxRef = meta['phx_ref'];
+
   final String phxRef;
-
-  /// Clones a [PhoenixPresenceMeta] object
-  PhoenixPresenceMeta clone() {
-    final json = _toJson();
-    return PhoenixPresenceMeta.fromJson(json);
-  }
-
-  Map<String, dynamic> _toJson() => data;
 }
